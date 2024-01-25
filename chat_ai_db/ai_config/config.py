3add733b6e7762924fe7cc3047aa43f505a0b619
ymd65536 <ymd65536@gmail.com>
2024-01-25 20:53:37 +0900

    modified:

--- chat_ai_db/ai_config/config.py
+++ chat_ai_db/ai_config/config.py
@@ -1,9 +1,12 @@
 import os
 
 PROJECT_ID = os.getenv("PROJECT_ID")
-ME_REGION = os.getenv("ME_REGION") # "asia-northeast1"
+
+# "asia-northeast1"
+ME_REGION = os.getenv("ME_REGION")
 ME_EMBEDDING_DIR = f"{PROJECT_ID}-me-bucket"
 GCS_BUCKET_DOCS = f"{PROJECT_ID}-documents"
+MODEL_NAME = "textembedding-gecko@001"
 
 doc_folder_prefix = "documents/samples/"
 index_folder_prefix = "index/db"
@@ -11,3 +14,6 @@ index_folder_prefix = "index/db"
 template = """
 sample
 """
+
+ME_INDEX_NAME = f"{PROJECT_ID}-me-sample"
+ME_DIMENSIONS = 768  # when using Vertex PaLM Embedding
--- chat_ai_db/create_db.py
+++ chat_ai_db/create_db.py
@@ -2,9 +2,36 @@ from ai_config import config as config
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import GCSDirectoryLoader
-from langchain_community.vectorstores import Chroma
-# from langchain_community.embeddings import VertexAIEmbeddings
 from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_community.vectorstores import MatchingEngine
+from utils.matching_engine_utils import MatchingEngineUtils
+from google.cloud import aiplatform
+
+# インデックス作成
+mengine = MatchingEngineUtils(
+    config.PROJECT_ID,
+    config.ME_REGION,
+    config.ME_INDEX_NAME
+)
+
+index = mengine.create_index(
+    embedding_gcs_uri=f"gs://{config.ME_EMBEDDING_DIR}/{config.index_folder_prefix}",
+    dimensions=config.ME_DIMENSIONS,
+    index_update_method="streaming",
+    index_algorithm="tree-ah",
+)
+if index:
+    print(index.name)
+
+index_endpoint = mengine.deploy_index()
+if index_endpoint:
+    print(f"Index endpoint resource name: {index_endpoint.name}")
+    print(
+        f"Index endpoint public domain name: {index_endpoint.public_endpoint_domain_name}"
+    )
+    print("Deployed indexes on the index endpoint:")
+    for d in index_endpoint.deployed_indexes:
+        print(f"    {d.id}")
 
 # GCS保存場所
 doc_folder_prefix = config.doc_folder_prefix
@@ -27,21 +54,31 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 split_documents = text_splitter.split_documents(documents)
+texts = [doc.page_content for doc in split_documents]
+metadatas = [
+    [
+        {"namespace": "source", "allow_list": [doc.metadata["source"]]},
+        {"namespace": "document_name", "allow_list": [doc.metadata["document_name"]]},
+        {"namespace": "chunk", "allow_list": [str(doc.metadata["chunk"])]},
+    ]
+    for doc in split_documents
+]
 
-# embeddingを初期化
-embeddings = VertexAIEmbeddings(
-    model="text-bison@001"
+ME_INDEX_ID, ME_INDEX_ENDPOINT_ID = mengine.get_index_and_endpoint()
+
+aiplatform.init(project=config.PROJECT_ID, location=config.ME_REGION)
+vector_store = MatchingEngine.from_components(
+    embedding=VertexAIEmbeddings(model_name=config.MODEL_NAME),
+    project_id=config.PROJECT_ID,
+    region=config.ME_REGION,
+    gcs_bucket_name=config.GCS_BUCKET_NAME,
+    index_id=ME_INDEX_ID,
+    endpoint_id=ME_INDEX_ENDPOINT_ID,
 )
 
-# Chromaを初期化
-chroma_db = Chroma(
-    persist_directory=f"gs://{config.ME_EMBEDDING_DIR}/{index_folder_prefix}",
-    embedding_function=embeddings
-)
+doc_ids = vector_store.add_texts(texts=texts, metadatas=metadatas)
 
-# ドキュメントを追加する
-chroma_db.add_documents(
-    split_documents
-)
+similar_doc = doc_ids.similarity_search("国民の祝日とはなんですか？", k=2)[0]
+print(similar_doc.page_content)
 
 print("Create Database")
